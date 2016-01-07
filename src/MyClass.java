af1b4406c3d77cf23ac794ae9437f6c83e399fe3
Kidslife <kidpres666@gmail.com>
2016-01-07 13:37:35 +0200

    added date output

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,9 +1,11 @@
+import java.util.*;
 
 public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, world!");
+		Date d = new Date();
+		System.out.println(d.toString() + ": Hello, world!");
 	}
 
 }
